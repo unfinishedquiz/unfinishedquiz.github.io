f8f863c2fc4e0674e5ef148fe7ff82a6cb933f08
unfinishedquiz <64604790+unfinishedquiz@users.noreply.github.com>
2020-05-03 21:54:47 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,13 @@
+'use strict';
+
 let q_no = 1
 let previous_answer = ''
 let language = 'en'
 let scrl_amount = {val: 0}
+let q_ctx = {}
 
-let parent_url_quiz = "https://unfinishedquiz.000webhostapp.com/qz/"
-let parent_url_res = "https://unfinishedquiz.000webhostapp.com/res/"
+let parent_url_quiz = "http://localhost/UnfinishedQuiz/qz/"
+let parent_url_res = "http://localhost/UnfinishedQuiz/res/"
 
 $(document).ready(() => {
 
@@ -26,7 +29,7 @@ $(document).ready(() => {
         })
     })
 
-    $.post(parent_url_quiz, {}, (data) => load_content(data))
+    if(!loadProgress()) $.post(parent_url_quiz, {}, (data) => load_content(data));
 
     $('#answer').keydown((e)=> {
         if (e.keyCode == 13) next();
@@ -38,6 +41,8 @@ $(document).ready(() => {
 })
 
 function giveup(){
+    saveProgress();
+
     $('.q-card').css('min-height', '0')
     $('.q-card').animate({height: 0, padding: '0 auto'}, 700)
     $('.q-card').animate({width: 0}, 300)
@@ -95,17 +100,18 @@ function incorrect_answer(){
     setTimeout(() => {
         hide_loading()
 
-        let incorrect_message = '<div class="cross"><div class="cross-line1-wrap">' +
+        let incorrect_message = 
+        '<div class="cross"><div class="cross-line1-wrap">' +
         '<div class="cross-line1"></div></div><div class="cross-line2-wrap">' + 
         '<div class="cross-line2"></div></div></div>'
 
-        $('#result').toggleClass('q-incorrect')
-        $('#result').html(incorrect_message)
-        animate_incorrect_message()
+        $('#result').toggleClass('q-incorrect');
+        $('#result').html(incorrect_message);
+        animate_incorrect_message();
         setTimeout(() => {
-            $('#result').html("")
-            $('#result').removeAttr('class')
-            $('#result').removeAttr('style')
+            $('#result').html("");
+            $('#result').removeAttr('class');
+            $('#result').removeAttr('style');
         }, 2400)
 
     }, 2000)
@@ -114,14 +120,14 @@ function incorrect_answer(){
 
 
 function next(){
-    let answer = $('#answer').val()
+    let answer = $.trim($('#answer').val()).toLowerCase();
     $('#answer').val("")
     q_ctx = {
         level: q_no, 
         answer: answer,
         language: language
     }
-    q_ctx_json = JSON.stringify(q_ctx)
+    let q_ctx_json = JSON.stringify(q_ctx)
     $.post(parent_url_quiz, {q_context: q_ctx_json}, 
         (data) => {
             if (data.status == "incorrect") {
@@ -139,42 +145,41 @@ function next(){
 }
 
 function changeLanguage(requestedLang){
-    q_ctx = {
-        level: q_no - 1, 
-        answer: previous_answer,
-        language: 'si'
-    }
-
-    $.post(parent_url_quiz, {q_context: q_ctx_json}, 
-        (data) => {
-            if (data.status == "incorrect") {
-                alert("Sorry, internal error occured!")
-                $('#answer').focus()
-            } else{
-                load_content(data)
-            }
-        }
-    )
+    language = requestedLang;
+    load_content(q_ctx)
 }
 
 function load_content(data){
-    window.scrollTo(0, 0)
-    scrl_amount.val = 0
+    q_ctx = data;
 
+    window.scrollTo(0, 0);
+    scrl_amount.val = 0;
+    q_ctx = data;
     q_no = data.level;
-    $('.q-level').html('Q. ' + q_no)
-    $('.q-content').html(data.question_en)
+    let q_content = (language === 'si') ? data.question_si : data.question_en;
+
+    $('.q-level').html('Q. ' + q_ctx.level);
+    $('.q-content').html(q_content);
 
     if(data.image){
-        $('.q-graphic').html('<img src=' + parent_url_res + q_no + '.png>')
-        $('.q-img-utilize').css('opacity', '1')
+        $('.q-graphic').html('<img src=' + parent_url_res + q_no + '.png>');
+        $('.q-img-utilize').css('opacity', '1');
     } else {
-        $('.q-graphic').html('<img src=' + parent_url_res +'empyt-img.png>')
-        $('.q-img-utilize').css('opacity', '0')
+        $('.q-graphic').html('<img src=' + parent_url_res +'empyt-img.png>');
+        $('.q-img-utilize').css('opacity', '0');
     }
     
-    $('#answer').focus()
-    $('#answer').val("")
+    $('#answer').focus();
+    $('#answer').val("");
+
+    anime({
+        targets: scrl_amount,
+        val: $('.q-content').offset().top,
+        update: () =>  window.scrollTo(0, scrl_amount.val),
+        easing: 'linear',
+        duration: 1500,
+        delay: 1000
+    })
 }
 
 function correct_answer(data){
@@ -192,4 +197,64 @@ function correct_answer(data){
         }, 1000)        
     } , 1000)
     
+}
+
+function saveProgress(){
+    let ctx = {
+        level: q_ctx.level - 1, 
+        answer: previous_answer,
+        language: language
+    };
+
+    if (ctx.level === 0) {
+        pop_message("Progress doesn't saved as you are still in 1st quiz.");
+        return;
+    }
+
+    $.post(parent_url_quiz, 
+        {q_context : JSON.stringify(ctx)}, 
+        (data) => {
+            if (data.status == 'correct') {
+                let progress = {
+                    level: q_ctx.level,
+                    previous_answer: previous_answer,
+                    language: language
+                };
+
+                let progress_cookie = JSON.stringify(progress);
+                progress_cookie = progress_cookie;
+
+                saveCookie('progress', progress_cookie, 2 * 30);
+            }
+            else progressSaveFailed();
+        })
+}
+
+function pop_message(message){
+    alert(message)
+}
+
+function progressSaveFailed(){
+
+}
+
+function checkSavability(){
+
+}
+
+function loadProgress(){
+    let progress_str = getCookie('progress');
+    if (progress_str == null) return false;
+
+    let progress_object = JSON.parse(progress_str);
+    let progress = {
+        level: progress_object.level - 1,
+        answer: progress_object.previous_answer,
+        language: 'en'
+    };
+
+    progress_str = JSON.stringify(progress);
+
+    $.post(parent_url_quiz, {q_context:progress_str}, (data) => load_content(data));
+    return true;
 }
